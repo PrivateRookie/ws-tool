8d5211f2587cca9f97f50755adcac1a7982ff0b7
PrivateRookie <996514515@qq.com>
2022-12-10 12:07:57 +0800

    fix: use muti thread as load test client

--- examples/load_test.rs
+++ examples/load_test.rs
@@ -1,9 +1,13 @@
-use std::{path::PathBuf, time};
+use std::{
+    path::PathBuf,
+    thread::JoinHandle,
+    time::{self, Duration},
+};
 
 use clap::Parser;
 use tracing::Level;
 use tracing_subscriber::util::SubscriberInitExt;
-use ws_tool::{codec::AsyncWsBytesCodec, ClientBuilder};
+use ws_tool::{codec::WsBytesCodec, ClientBuilder};
 
 /// websocket client demo with raw frame
 #[derive(Parser)]
@@ -14,7 +18,7 @@ struct Args {
     #[clap(long, default_value = "1")]
     conn: usize,
 
-    /// payload size kb
+    /// payload size (unit: kb)
     #[clap(short, long, default_value = "1")]
     payload: usize,
 
@@ -27,45 +31,60 @@ struct Args {
     cert: Option<PathBuf>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), ()> {
+fn main() -> Result<(), ()> {
     tracing_subscriber::fmt::fmt()
         .with_max_level(Level::INFO)
         .finish()
         .try_init()
         .expect("failed to init log");
     let args = Args::parse();
-    let mut builder = ClientBuilder::new(&args.uri);
-    if let Some(cert) = args.cert {
-        builder = builder.cert(cert);
-    }
+
     // if let Some(proxy) = args.proxy {
     //     builder = builder.proxy(&proxy)
     // }
     let total = args.num;
     let size = args.payload;
-    let mut f = vec![];
-    for _ in 0..args.conn {
-        let mut client = builder
-            .async_connect(AsyncWsBytesCodec::check_fn)
-            .await
-            .unwrap();
-        let j = tokio::spawn(async move {
-            let start = time::SystemTime::now();
-            let mut payload = vec![0].repeat(size * 1024);
-            for _ in 0..total {
-                client.send(&mut payload[..]).await.unwrap();
-                client.receive().await.unwrap();
+    let handlers: Vec<JoinHandle<Duration>> = (0..args.conn)
+        .map(|_| {
+            let uri = args.uri.clone();
+            let cert = args.cert.clone();
+            std::thread::spawn(move || {
+                let mut builder = ClientBuilder::new(uri);
+                if let Some(cert) = cert {
+                    builder = builder.cert(cert);
+                }
+                let mut client = builder.connect(WsBytesCodec::check_fn).unwrap();
+                client.stream_mut().stream_mut().set_nodelay(true).unwrap();
+                let start = time::SystemTime::now();
+                let mut payload = vec![0].repeat(size * 1024);
+                for _ in 0..total {
+                    client.send(&mut payload[..]).unwrap();
+                    client.receive().unwrap();
+                }
+                let end = time::SystemTime::now();
+                let elapse = end.duration_since(start);
+                elapse.unwrap()
+            })
+        })
+        .collect();
+    let mut success = vec![];
+    let mut failed = vec![];
+    for (idx, h) in handlers.into_iter().enumerate() {
+        match h.join() {
+            Ok(elapse) => {
+                success.push((idx, elapse));
             }
-            let end = time::SystemTime::now();
-            let elapse = end.duration_since(start);
-            elapse
-        });
-        f.push(j)
+            Err(e) => {
+                failed.push((idx, e));
+            }
+        }
     }
-    let all = futures::future::join_all(f).await;
-    for i in all {
-        println!("{}", total as f64 / i.unwrap().unwrap().as_secs_f64())
+    for (idx, s) in success {
+        println!("[{idx:0>3} - OK] {:.3}", total as f64 / s.as_secs_f64());
     }
+    for (idx, e) in failed {
+        println!("[{idx:0>3} - ERR] {e:?}")
+    }
+
     Ok(())
 }
